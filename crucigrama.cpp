fc89b78d70d50481fdbcd888da170fec63f532d8
duckfuker69 <61020584+duckfuker69@users.noreply.github.com>
2020-02-29 13:40:30 -0600

    aH
    No sé

---
+++ crucigrama.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char palabra1[31],palabra2[31];
+
+  printf("\nIntroduce la primera y segunda palabra");
+  scanf("%s,%s",palabra1, palabra2 );
+
+
+
+
+  return 0;
+}
