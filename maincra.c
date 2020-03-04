4396a4990e8bdf144093d0c92b39219fe38490cb
duckfuker69 <61020584+duckfuker69@users.noreply.github.com>
2020-03-04 14:47:16 -0600

    Correciones

--- maincra.c
+++ maincra.c
@@ -3,7 +3,7 @@
 
 int main() {
   int aguante, num_filas, diamantes[1000], acumulado=0;
-  int suma1[30];
+  int suma1[600];
   scanf("%d",&num_filas );//se pide numero de filas
 
   printf("Ingrese los diamantes en cada fila");
@@ -14,19 +14,19 @@ int main() {
   printf("Ingrese el aguante del pico");
   scanf("%d",&aguante ); //se pide el aguante del pico
 
-  for (size_t i = 0; i < 30; i++)// for para incializar el arreglo suma en 0
+  for (size_t i = 0; i < 600; i++)// for para incializar el arreglo suma en 0
   {
     suma1[i]=0;
   }
   for (size_t i = 0; i < num_filas; i++)//for para el numero de elemento del array en suma1
   {
-    for ( j = 0; j < aguante; j++)//for para que se recorra el arreglo diamantes
+    for (int j = 0; j < aguante; j++)//for para que se recorra el arreglo diamantes
     {
       suma1[i]=suma1[i]+diamantes[j+i];//#aguante sumas de 0 + valor de diamantes en j+i posicion (se recorre gracias al +i)que se almacenara en suma1 dependiendo de i
     }
 
   }
-  for (size_t i = 0; i < 30; i++)//for para comparar los valores en suma1
+  for (size_t i = 0; i < 600; i++)//for para comparar los valores en suma1
   {
       if (suma1[i]>suma1[i+1]&&acumulado<suma1[i+1]) { //if que recorre el arreglo y compara los valores
         acumulado=suma1[i]; //se asigna a acumulado el valor más alto
