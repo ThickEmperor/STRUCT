520afaeff1d786b3a31d3cf592775aedb1a1dfa0
duckfuker69 <61020584+duckfuker69@users.noreply.github.com>
2020-03-04 09:42:41 -0600

    Agregué crucigrama

--- crucigrama.cpp
+++ crucigrama.cpp
@@ -1,11 +1,48 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main() {
-  char palabra1[31],palabra2[31];
+  int aguante, num_filas, diamantes[1000],suma1,suma2, acumulado=0;
 
-  printf("\nIntroduce la primera y segunda palabra");
-  scanf("%s,%s",palabra1, palabra2 );
+  scanf("%d",&num_filas );
+  printf("Ingrese los diamantes en cada fila");
+  for (int i = 0; i < num_filas; i++) //Se pi
+  {
+    scanf("%d",&diamantes[i] );
+  }
+  printf("Ingrese el aguante del pico");
+  scanf("%d",&aguante );
+
+  for (int i = 0; i < aguante; i++)
+  {
+    suma2=0;
+    for (int j = 0; j <= aguante; j++)
+    {
+    suma1=diamantes[j]+diamantes[j+1];
+    suma2=suma1+suma2;
+    printf("\nsuma%d\n",suma2 );
+    if (i>=1)
+    {
+      j++;
+    }
+    }
+
+
+
+    if (suma2>acumulado)
+    {
+      acumulado=suma2;
+    }
+    printf("\nacumulado%d\n",acumulado );
+
+
+
+  }
+
+
 
 
 
