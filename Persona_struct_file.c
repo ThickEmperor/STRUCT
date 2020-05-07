284e8f6ee733c9435b9cfe41480c7e5d22f1c7ee
duckfuker69 <61020584+duckfuker69@users.noreply.github.com>
2020-05-07 10:32:47 -0500

    indentado jeje

--- Persona_struct_file.c
+++ Persona_struct_file.c
@@ -24,56 +24,53 @@ int main()
     aarchivo = fopen("salida.bin", "rb");
     if(aarchivo == NULL)
     {
-        printf("Error al abrir archivo.\n");
+      printf("Error al abrir archivo.\n");
 
 
 
-    for (size_t i = 0; i < 2; i++)
-    {
-      printf("\n\n\n");
-      array_de_personas[i].id_persona = i+1;
-      printf("Introduzca su nombre completo:\n");
-      fgets(array_de_personas[i].nombre, 99,stdin);
-      printf("Introduzca el sexo de la persona [H/M]:\n");
-      array_de_personas[i].sexo = getc(stdin);
-      while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
-      printf("Introduzca la direccion:\n");
-      fgets(array_de_personas[i].direccion, 99,stdin);
-      printf("Introduzca la religion:\n");
-      fgets(array_de_personas[i].religion, 49,stdin);
-      printf("Introduzca la escolaridad:\n");
-      fgets(array_de_personas[i].escolaridad, 19,stdin);
-      printf("Introduzca la edad:\n");
-      scanf("%hu", &array_de_personas[i].edad);
-      while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+	    for (size_t i = 0; i < 2; i++)
+	    {
+	      printf("\n\n\n");
+	      array_de_personas[i].id_persona = i+1;
+	      printf("Introduzca su nombre completo:\n");
+	      fgets(array_de_personas[i].nombre, 99,stdin);
+	      printf("Introduzca el sexo de la persona [H/M]:\n");
+	      array_de_personas[i].sexo = getc(stdin);
+	      while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+	      printf("Introduzca la direccion:\n");
+	      fgets(array_de_personas[i].direccion, 99,stdin);
+	      printf("Introduzca la religion:\n");
+	      fgets(array_de_personas[i].religion, 49,stdin);
+	      printf("Introduzca la escolaridad:\n");
+	      fgets(array_de_personas[i].escolaridad, 19,stdin);
+	      printf("Introduzca la edad:\n");
+	      scanf("%hu", &array_de_personas[i].edad);
+	      while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
 
-    aarchivo = fopen("salida.bin", "wb");
-    if(aarchivo == NULL)
-    {
+	    aarchivo = fopen("salida.bin", "wb");
+	    if(aarchivo == NULL)
+    	{
         printf("Error al abrir archivo.\n");
         return(1);
-    }
-    }
-  fwrite(array_de_personas, sizeof(Persona), 2, aarchivo);
+    	}
+  	}
+  	fwrite(array_de_personas, sizeof(Persona), 2, aarchivo);
 
-  }
-  else
-	{
-    fread(array_de_personas, sizeof(Persona),2, aarchivo);
-
-
-    for (size_t i = 0; i < 2; i++)
+  	}
+  	else
 		{
+    	fread(array_de_personas, sizeof(Persona),2, aarchivo);
+    	for (size_t i = 0; i < 2; i++)
+			{
+      	printf("\n%lu\n%s\n%c\n%s\n%s\n%s\n%hu\n", array_de_personas[i].id_persona, array_de_personas[i].nombre, array_de_personas[i].sexo, array_de_personas[i].direccion, array_de_personas[i].religion, array_de_personas[i].escolaridad, array_de_personas[i].edad);
+    	}
 
-      printf("\n%lu\n%s\n%c\n%s\n%s\n%s\n%hu\n", array_de_personas[i].id_persona, array_de_personas[i].nombre, array_de_personas[i].sexo, array_de_personas[i].direccion, array_de_personas[i].religion, array_de_personas[i].escolaridad, array_de_personas[i].edad);
-    }
-
-	}
+		}
 
 
     /*for (size_t i = 0; i < 2; i++) {
       printf("%s\n%c\n%s\n%s\n%s\n%d",array_de_personas[i].nombre,array_de_personas[i].sexo, array_de_personas[i].direccion,array_de_personas[i].religion,array_de_personas[i].escolaridad, array_de_personas[i].edad );
     }*/
 
-    fclose(aarchivo);
+  fclose(aarchivo);
 }
